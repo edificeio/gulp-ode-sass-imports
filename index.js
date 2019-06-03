f75e654b32830792298504fea1382e9f75993b8a
Simon Basset <simon.basset@gmail.com>
2019-06-03 18:05:30 +0200

    refactor: optimization

--- index.js
+++ index.js
@@ -1,55 +1,58 @@
-// through2 is a thin wrapper around node transform streams
-var through = require('through2');
-var gutil = require('gulp-util');
-var fs = require('fs');
-var glob = require("glob");
-var PluginError = gutil.PluginError;
-var os = require('os');
+const through = require('through2');
+const gutil = require('gulp-util');
+const fs = require('fs');
+const glob = require('glob');
+const PluginError = gutil.PluginError;
+const os = require('os');
 
-function sassImports(baseTheme, cb){
-    var psText = '';
-    let n = 2;
-    function addFiles(err, files){
-        files.forEach(file => {
-            file = file .split('.scss')[0].split('_')[0] + file.split('.scss')[0].split('_')[1];
-            file = '../../../../../' + file;
-            psText += '@import "' + file + '";' + os.EOL;
-        });
+function memoize(func) {
+    const memoized = function () {
+        const args = arguments,
+            key = args[0],
+            cache = memoized.cache;
 
-        n--;
-        if(n === 0){
-            cb(new Buffer(psText));
+        if (cache.has(key)) {
+            return cache.get(key);
         }
-    }
-    
-    glob('./mods/**/public/sass/global/*.scss', addFiles);
-    glob('./mods/**/public/sass/' + baseTheme + '/*.scss', addFiles);
+        const result = func.apply(this, args);
+        memoized.cache = cache.set(key, result) || cache;
+        return result;
+    };
+    memoized.cache = new Map();
+    return memoized;
 }
 
-function appendVersion(contents){
-    return contents.split('</body>').slice(0, 1).join('') + 
-        '<script>' + os.EOL + 'var springboardBuildDate="' + new Date().toISOString() + '"' + os.EOL + '</script>' + os.EOL + 
+function generateImportForBaseTheme(baseTheme) {
+    const files = glob.sync('./mods/**/public/sass/@(global|' + baseTheme + ')/*.scss');
+    return files.map(file => {
+        let rfile = file.split('.scss')[0].split('_')[0] + file.split('.scss')[0].split('_')[1];
+        rfile = '../../../../../' + rfile;
+        return '@import "' + rfile + '";';
+    }).join(os.EOL);
+}
+
+const memoizedGenerateImportForBaseTheme = memoize(generateImportForBaseTheme);
+
+
+function appendVersion(contents) {
+    return contents.split('</body>').slice(0, 1).join('') +
+        '<script>' + os.EOL + 'var springboardBuildDate="' + new Date().toISOString() + '"' + os.EOL + '</script>' + os.EOL +
         '</body></html>';
 }
 
 function stringToStream(text) {
-  var stream = through();
-  stream.write(text);
-  return stream;
+    const stream = through();
+    stream.write(text);
+    return stream;
 }
 
 // Plugin level function(dealing with files)
-function gulpSassImports(bt) {
-    baseTheme = bt;
-    return through.obj(function(file, enc, cb) {
-        
-        if(file.path.indexOf('.html') !== -1){
-            
-            if (file.isNull()) {
-                // return empty file
-                return cb(null, file);
-            }
-
+function gulpSassImports(baseTheme) {
+    return through.obj(function (file, enc, cb) {
+        if (file.isNull()) {
+            return cb(null, file);
+        }
+        if (file.path.indexOf('.html') !== -1) {
             const newFileContents = appendVersion(file.contents.toString());
             if (file.isBuffer()) {
                 file.contents = Buffer.from(newFileContents, 'utf8');
@@ -59,25 +62,19 @@ function gulpSassImports(bt) {
             }
 
             return cb(null, file);
-        }
-        else{
-            sassImports(bt, function(generatedImports){
-                if (file.isNull()) {
-                    // return empty file
-                    return cb(null, file);
-                }
-                if (file.isBuffer()) {
-                    file.contents = Buffer.concat([file.contents, generatedImports]);
-                }
-                if (file.isStream()) {
-                    file.contents = file.contents.pipe(stringToStream(generatedImports));
-                }
-    
-                return cb(null, file);
-            });
+        } else {
+            const generatedImports = new Buffer(memoizedGenerateImportForBaseTheme(baseTheme));
+            if (file.isBuffer()) {
+                file.contents = Buffer.concat([file.contents, generatedImports]);
+            }
+            if (file.isStream()) {
+                file.contents = file.contents.pipe(stringToStream(generatedImports));
+            }
+
+            return cb(null, file);
         }
     });
 }
 
-// Exporting the plugin main function
 module.exports = gulpSassImports;
+
